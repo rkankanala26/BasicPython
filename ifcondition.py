baee30ad8c1aac5c761d176dfe9f646a6abc8f1d
rkankanala26 <rkankanala26@gmail.com>
2020-07-03 21:40:35 -0700

    Python Homework 1

---
+++ ifcondition.py
@@ -0,0 +1,9 @@
+x=21
+
+if x< 13: 
+    print("you cannot get driver permit")
+    print("yes you are still kid")
+    print("come back when you are 18")
+
+else:
+        print(" you can get driver permit")
